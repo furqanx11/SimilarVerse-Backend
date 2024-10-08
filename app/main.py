f36f26165ad28b895313eba06f19addc1e7d8604
Furqan Ahmed <ahmedfurqan510@gmail.com>
2024-10-08 18:17:00 +0500

    Added CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, FastAPI
 from app.utils.data_ingestion import main
+from app.middleware.cors import cors_middleware
 
 main()
 
 from app.routers import routes
-app = APIRouter()
+app = FastAPI()
+cors_middleware(app)
 app.include_router(routes.router)
