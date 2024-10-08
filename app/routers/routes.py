7e557e3ada3383e42af8ccde89783c07a5f28286
Furqan Ahmed <ahmedfurqan510@gmail.com>
2024-10-08 18:25:23 +0500

    Fixed issues in fetching verses for verse number

--- app/routers/routes.py
+++ app/routers/routes.py
@@ -7,17 +7,19 @@ router = APIRouter()
 
 @router.post("/")
 def get_verse_by_number(number: VerseNumber):
-    try:
-        response = get_similar_verse(number=number.dict())
-        return {
-            "Arabic": response[0],
-            "EnglishTranslation": response[1],
-            "UrduTranslation": response[2],
-            "SurahNo": response[3],
-            "AyahNo": response[4]
-        }
-    except IndexError:
+    response = get_similar_verse(number=number.dict())
+    if len(response) == 0:
         raise HTTPException(status_code=404, detail="Verse not found")
+    return [
+        {
+            "Arabic": res[0],
+            "EnglishTranslation": res[1],
+            "UrduTranslation": res[2],
+            "SurahNo": res[3],
+            "AyahNo": res[4]
+        }
+        for res in response
+    ]
 
 @router.post("/arabic")
 def get_verse_by_arabic(arabic: ArabicText):
--- app/utils/similar_verse.py
+++ app/utils/similar_verse.py
@@ -4,8 +4,8 @@ quran_compiled = pd.read_csv('data/QuranCompiled.csv')
 
 def get_similar_verse(number=None, arabic=None):
     if number:
-        response = quran_compiled[(quran_compiled['SurahNo'] == number['SurahNo']) & (quran_compiled['AyahNo'] == number['AyahNo'])][['Arabic', 'EnglishTranslation', 'UrduTranslation', 'SurahNo', 'AyahNo']].values[0]
-        return response
+        verse = quran_compiled[(quran_compiled['SurahNo'] == number['SurahNo']) & (quran_compiled['AyahNo'] == number['AyahNo'])][['Arabic']].values[0]
+        return get_similar_verse(arabic=verse[0])
     elif arabic:
         # Debugging: Print the Arabic text being searched
         print(f"\nSearching for Arabic text: {arabic}")
